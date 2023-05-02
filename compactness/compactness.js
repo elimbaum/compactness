b524308065e6102f2a013868bda3e26e882c6994
Eli Baum <elimbaum@gmail.com>
2023-05-01 21:47:39 -0400

    display stats; tolerate minimal color differences

--- compactness/compactness.js
+++ compactness/compactness.js
@@ -1,7 +1,7 @@
-const STROKE_WEIGHT = 10;
+const STROKE_WEIGHT = 5;
 const PIXEL_DENSITY = 2;
 
-const DEFAULT_FILL = 127;
+const DEFAULT_FILL = 200;
 
 let map;
 
@@ -11,10 +11,10 @@ function setupFormat() {
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(canvasWidth, canvasHeight);
   pixelDensity(PIXEL_DENSITY);
 
-  map = createGraphics(windowWidth, windowHeight);
+  map = createGraphics(width, height);
   map.pixelDensity(PIXEL_DENSITY);
 
   map.background(0);
@@ -29,6 +29,9 @@ function setup() {
 
 let lastP = null;
 let isDrawing = false;
+let isCalculating = false;
+
+let showStats = false;
 
 function getStatusText() {
   s = "";
@@ -36,6 +39,15 @@ function getStatusText() {
     s += "drawing";
   } else {
     s += "viewing";
+  }
+  
+  if (isCalculating) {
+    s += "\ncalculating...";
+  }
+  
+  if (showStats) {
+    c = compactness;
+    s += `\narea: ${count}\nperim: ${perimeter.toFixed(2)}\nc: ${c.toFixed(2)}\n1/c: ${(1/c).toFixed(2)}`
   }
 
   return s;
@@ -45,6 +57,7 @@ function draw() {
   background(0);
 
   if (isDrawing) {
+    //showStats = false;
     let p = new Point(mouseX, mouseY);
     if (lastP != p) {
       map.line(lastP.x, lastP.y, p.x, p.y);
@@ -54,6 +67,12 @@ function draw() {
 
   image(map, 0, 0, width, height);
   text(getStatusText(), 10, 20);
+  
+  push();
+  strokeWeight(2);
+  stroke('red');
+  edge.forEach(e => point(e.x, e.y));
+  pop();
 }
 
 function mouseClicked() {
@@ -66,15 +85,26 @@ function mouseClicked() {
 let ERASE_KEY = 'x';
 let FILL_KEY = 'f';
 let CALC_KEY = 'c';
+let INSPECT_KEY = 'i';
 
 function keyPressed() {
   if (key == ERASE_KEY) {
+    showStats = false;
     map.background(0);
+    edge = [];
     isDrawing = false;
   } else if (key == FILL_KEY) {
+    //showStats = false;
     floodFill(map, new Point(mouseX, mouseY));
   } else if (key == CALC_KEY) {
+    isCalculating = true;
     calculate(map, new Point(mouseX, mouseY));
+    isCalculating = false;
+    showStats = true;
+  } else if (key == INSPECT_KEY) {
+    x = mouseX;
+    y = mouseY;
+    print("color @", x, y, map.get(x, y));
   }
 
   setupFormat();
--- compactness/floodFill.js
+++ compactness/floodFill.js
@@ -5,13 +5,20 @@
 let frontier = [];
 let edge = [];
 let count = 0;
+let perimeter = 0;
+let compactness = 0;
 
 let history = new Set();
 
-let FILL;
+function isNear(a, b, limit) {
+  return abs(a - b) <= limit;
+}
+
+// no idea why we need this.
+let COLOR_LIMIT = 2;
 
 function checkPixel(map, p) {
-  
+
   // https://p5js.org/reference/#/p5/pixels
   if (history.has(p.toKey())) {
     return false;
@@ -24,8 +31,10 @@ function checkPixel(map, p) {
   _b = map.pixels[idx + 1];
   _g = map.pixels[idx + 2];
   _a = map.pixels[idx + 3];
-  isEmpty = (_r == 0 && _g == 0 && _b == 0);
-
+  isEmpty = (isNear(_r, 0, COLOR_LIMIT) && 
+              isNear(_g, 0, COLOR_LIMIT) &&
+              isNear(_b, 0, COLOR_LIMIT));
+              
   if (isEmpty) {
     map.set(p.x, p.y, DEFAULT_FILL);
     frontier.push(p);
@@ -47,7 +56,9 @@ function checkFilled(map, p) {
   _b = map.pixels[idx + 1];
   _g = map.pixels[idx + 2];
   _a = map.pixels[idx + 3];
-  isFilled = (_r != 0 && _g != 0 && _b != 0);
+  isFilled = (isNear(_r, DEFAULT_FILL, COLOR_LIMIT) && 
+              isNear(_g, DEFAULT_FILL, COLOR_LIMIT) &&
+              isNear(_b, DEFAULT_FILL, COLOR_LIMIT));
 
   if (isFilled) {
     count += 1;
@@ -76,50 +87,57 @@ function floodFill(map, p) {
 
     checkPixel(map, new Point(p.x + 1, p.y    ));
     checkPixel(map, new Point(p.x - 1, p.y    ));
-    checkPixel(map, new Point(p.x    , p.y + 1));
-    checkPixel(map, new Point(p.x    , p.y - 1));
+    checkPixel(map, new Point(p.x, p.y + 1));
+    checkPixel(map, new Point(p.x, p.y - 1));
   }
-  
+
   map.updatePixels();
-  
+
   // fill in the edge
   edge.forEach(e => map.point(e.x, e.y));
-  
-  print("Finish Flood Fill");
+  edge = []
+
+    print("Finish Flood Fill");
 }
 
-function computePerimeter(edge) {
-  // lazy version
+function computePerimeter(edge) { 
+  // sort by x
+  edge.sort((a, b) => (a.x - b.x));
+  
+  print(edge);
+  
   return edge.length;
 }
 
-function calculate(map, p) {  
+function calculate(map, p) {
   frontier = [];
   edge = [];
   count = 0;
-  
+
   frontier.push(p);
-  
+
   history.clear();
   history.add(p);
-  
+
   map.loadPixels();
-  
+
+
   while (frontier.length) {
     p = frontier.pop();
 
     checkFilled(map, new Point(p.x + 1, p.y    ));
     checkFilled(map, new Point(p.x - 1, p.y    ));
-    checkFilled(map, new Point(p.x    , p.y + 1));
-    checkFilled(map, new Point(p.x    , p.y - 1));
+    checkFilled(map, new Point(p.x, p.y + 1));
+    checkFilled(map, new Point(p.x, p.y - 1));
   }
-  
+
   map.updatePixels();
-  
+
   print("count", count);
-  print("edge", edge);
+  //print("edge", edge);
   perimeter = computePerimeter(edge)
   compactness = (4 * PI * count) / (perimeter * perimeter);
   print("shitty compactness", compactness);
- 
+
+  return edge;
 }
